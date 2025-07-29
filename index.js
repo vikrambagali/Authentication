98c71ab65f1a557326a900d29b3099bf2baa6950
vikrambagali <155137486+vikrambagali@users.noreply.github.com>
2025-07-29 20:02:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const validator = require("validator");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const saltRounds = 10;
+const MongoStore = require('connect-mongo');
+const helmet = require('helmet');
+
 
 const app = express();
+
+app.use(helmet());
+app.use(session({
+  secret: "thisisasecret",
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  cookie: { httpOnly: true }
+}));
 
 // View engine and static files
 app.set("view engine", "ejs");
@@ -24,16 +38,7 @@ app.use(
 );
 
 // ===== Connect to MongoDB =====
-mongoose.connect(
-  "mongodb://28vikram20:1BVYoFsnDvYz3G3k@" +
-  "ac-yourcluster-shard-00-00.dgblvhy.mongodb.net:27017," +
-  "ac-yourcluster-shard-00-01.dgblvhy.mongodb.net:27017," +
-  "ac-yourcluster-shard-00-02.dgblvhy.mongodb.net:27017/secrets" +
-  "?ssl=true&replicaSet=atlas-xxxx-shard-0&authSource=admin&retryWrites=true&w=majority"
-)
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("Connection error:", err));
-
+mongoose.connect("mongodb+srv://28vikram20:Vikram123@cluster0.dgblvhy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 // ===== User Schema and Model =====
 const userSchema = new mongoose.Schema({
   email: String,
@@ -80,15 +85,20 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-
-  const user = await User.findOne({ email: username });
-  if (user && await bcrypt.compare(password, user.password)) {
-    req.session.userId = user._id;
-    res.redirect("/secrets");
-  } else {
-    res.send("❌ Invalid email or password.");
+  try {
+    const user = await User.findOne({ email: username });
+    if (user && await bcrypt.compare(password, user.password)) {
+      req.session.userId = user._id;
+      res.redirect("/secrets");
+    } else {
+      res.send("❌ Invalid email or password.");
+    }
+  } catch (err) {
+    console.error(err);
+    res.send("❌ Login failed.");
   }
 });
+
 
 app.get("/secrets", async (req, res) => {
   if (req.session.userId) {
